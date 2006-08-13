fa5a202fb86a69cf89a06fcfe65bc5c6fe80eccf
ddossot <ddossot@3573d164-ba16-0410-8823-8bc0bb1327a7>
2006-08-13 11:38:13 +0000

    commented

--- NxBRE3/Source/Util/HashCodeBuilder.cs
+++ NxBRE3/Source/Util/HashCodeBuilder.cs
@@ -5,18 +5,34 @@ namespace NxBRE.Util
 	/// <summary>
 	/// A helper object for building hashcode based certain objects that belongs to a particular class.
 	/// </summary>
+	/// <remarks>
+	/// This helper follows, as much as possible in C#, Joshua Bloch's Effective Java recommandations on computing hashcode.
+	/// </remarks>
 	public class HashCodeBuilder
 	{
 		private int hashCode;
 		
+		/// <summary>
+		/// Creates a new HashCodeBuilder initialized for computing a new hashcode.
+		/// </summary>
 		public HashCodeBuilder() {
 			hashCode = 17;
 		}
 		
+		/// <summary>
+		/// Creates a new HashCodeBuilder initialized for extending an existing hashcode.
+		/// </summary>
+		/// <param name="hashCode">An actual existing hashcode.</param>
+		/// <remarks>Do not call this method with a random number, if you do not have an existing hashcode use the empty constructor.</remarks>
 		public HashCodeBuilder(int hashCode) {
 			this.hashCode = hashCode;
 		}
 		
+		/// <summary>
+		/// Append a new object (usually an object's field) to the current hashcode.
+		/// </summary>
+		/// <param name="o"></param>
+		/// <returns></returns>
 		public HashCodeBuilder Append(object o) {
 			if (o != null) {
 				long temp = Math.BigMul(37, hashCode);
@@ -26,6 +42,9 @@ namespace NxBRE.Util
 			return this;
 		}
 		
+		/// <summary>
+		/// The current hashcode value computed by this instance of HashCodeBuilder.
+		/// </summary>
 		public int Value {
 			get {
 				return hashCode;
