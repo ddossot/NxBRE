bbd8ce299b796bc41018e6049a124bb8758a6732
ddossot <ddossot@3573d164-ba16-0410-8823-8bc0bb1327a7>
2007-07-13 05:53:30 +0000

    BUG-1713544: created unit test and LegacySchedule/SmartSchedule switch

--- Source/InferenceEngine/IEImpl.cs
+++ Source/InferenceEngine/IEImpl.cs
@@ -875,7 +875,7 @@ namespace NxBRE.InferenceEngine {
 				iteration++;
 
 				// Schedule all implications matching the existing facts
-				agenda.Schedule(iterationPositiveImplications, IB);
+				agenda.Schedule(iterationPositiveImplications, IB, WM.FB);
 				agenda.PrepareExecution();
 				
 				if (Logger.IsInferenceEngineVerbose) Logger.InferenceEngineSource.TraceEvent(TraceEventType.Verbose, 0, "Iteration #" +
