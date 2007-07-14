6c2396b0c68a752c0508711658e33fed4fe60ab5
ddossot <ddossot@3573d164-ba16-0410-8823-8bc0bb1327a7>
2007-07-14 19:49:30 +0000

    added test for modifying implication events

--- Test/InferenceEngine/TestEventContext.cs
+++ Test/InferenceEngine/TestEventContext.cs
@@ -12,18 +12,29 @@ namespace NxBRE.Test.InferenceEngine {
 	
 	[TestFixture]
 	public class TestEventContext {
-		//TODO test counting implications event context
-		
 		private string ruleFilesFolder = Parameter.GetString("unittest.ruleml.inputfolder") + "/";
 		
 		private IEImpl ie;
 		
-		[TestFixtureSetUp]
+		private int newFactEventCount;
+		
+		private int deleteFactEventCount;
+		
+		private int modifyFactEventCount;
+		
+		private int factEventCount;
+		
+		[SetUp]
 		public void Setup() {
 			ie = new IEImpl();
 			
 			ie.LoadRuleBase(new RuleML09NafDatalogAdapter(ruleFilesFolder + "events-test.ruleml",
 			                                           FileAccess.Read));
+			
+			newFactEventCount = 0;
+			deleteFactEventCount = 0;
+			modifyFactEventCount = 0;
+			factEventCount = 0;
 		}
 		
 		[Test]
@@ -35,19 +46,36 @@ namespace NxBRE.Test.InferenceEngine {
 			ie.ModifyFactHandler += new NewFactEvent(ModifyFactHandlerWithContext);
 			
 			ie.Process();
+			
+			Assert.AreEqual(2, newFactEventCount);
+			Assert.AreEqual(1, deleteFactEventCount);
+			Assert.AreEqual(1, modifyFactEventCount);
 		}
 		
 		private void AssertFactHandlerWithContext(NewFactEventArgs nfea) {
-			Assert.AreEqual("Asserting Implication", nfea.Context.Implication.Label);
-			Assert.AreEqual("((trigger{whatever}))", Misc.IListToString(nfea.Context.Facts));
+			newFactEventCount++;
+			
+			string label = nfea.Context.Implication.Label;
+			
+			if (label == "Asserting Implication") {
+				Assert.AreEqual("((trigger{whatever}))", Misc.IListToString(nfea.Context.Facts));
+			} else if (label == "Counting Implication") {
+				Assert.AreEqual("((toCount{a}),(toCount{b}),(toCount{c}))", Misc.IListToString(nfea.Context.Facts));
+			} else {
+				Assert.Fail("Unexpected implication label: " + label);
+			}
 		}
 		
 		private void RetractFactHandlerWithContext(NewFactEventArgs nfea) {
+			deleteFactEventCount++;
+			
 			Assert.AreEqual("Retracting Implication", nfea.Context.Implication.Label);
 			Assert.AreEqual("((trigger{whatever}))", Misc.IListToString(nfea.Context.Facts));
 		}
 		
 		private void ModifyFactHandlerWithContext(NewFactEventArgs nfea) {
+			modifyFactEventCount++;
+			
 			Assert.AreEqual("Modifying Implication", nfea.Context.Implication.Label);
 			Assert.AreEqual("((toModify{whatever}))", Misc.IListToString(nfea.Context.Facts));
 		}
@@ -56,14 +84,18 @@ namespace NxBRE.Test.InferenceEngine {
 		public void TestAllActionsWithoutContext() {
 			ie.ExposeEventContext = false;
 			
-			ie.NewFactHandler +=  new NewFactEvent(EventHandlerWithoutContext);
+			ie.NewFactHandler += new NewFactEvent(EventHandlerWithoutContext);
 			ie.DeleteFactHandler += new NewFactEvent(EventHandlerWithoutContext);
 			ie.ModifyFactHandler += new NewFactEvent(EventHandlerWithoutContext);
 			
 			ie.Process();
+			
+			Assert.AreEqual(4, factEventCount);
 		}
 		
 		private void EventHandlerWithoutContext(NewFactEventArgs nfea) {
+			factEventCount++;
+			
 			Assert.IsNull(nfea.Context);
 		}
 		
