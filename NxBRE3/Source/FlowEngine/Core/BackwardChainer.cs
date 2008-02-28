a18ee962df148d6a80639aea24c1e668b1c986be
ddossot <ddossot@3573d164-ba16-0410-8823-8bc0bb1327a7>
2008-02-28 06:46:47 +0000

    FR-1776382 Added BackwardChainer to support RRML.

---
+++ NxBRE3/Source/FlowEngine/Core/BackwardChainer.cs
@@ -0,0 +1,55 @@
+﻿namespace NxBRE.FlowEngine.Core {
+	using System;
+	using System.Collections.Generic;
+	using System.Xml.XPath;
+
+	/// <summary>
+	/// TODO Description of BackwardChainer.
+	/// Schedules processing of sets based on a desired outcome (i.e. the id of context object that must be asserted).
+	/// </summary>
+	internal sealed class BackwardChainer {
+		private readonly IFlowEngine flowEngine;
+		
+		public BackwardChainer(IFlowEngine flowEngine) {
+			this.flowEngine = flowEngine;
+			
+			// TODO ensure the loaded rules are backwards chainable (no rules outside of sets for ex., no delegates?)
+		}
+		
+		public object Resolve(string id) {
+			// FIXME implement process all the scheduled sets until the sought id is found in context
+			return null;
+		}
+		
+		internal IList<string> GetSetIdsFromTargetObjectId(string id) {
+			IList<string> ids = new List<string>();
+			
+			XPathNodeIterator setIdAttributes = flowEngine.XmlDocumentRules.CreateNavigator().Select("//Set[.//Logic//Do//Rule[@id='"
+			                                                                                         + id
+			                                                                                         + "']/@id | .//Logic//Else//Rule[@id='"
+			                                                                                         + id
+			                                                                                         + "']]/@id");
+			while(setIdAttributes.MoveNext()) {
+				ids.Add(setIdAttributes.Current.Value);
+			}
+			
+			return ids;
+		}
+		
+		internal IList<string> GetSourceObjectIdsFromSetId(string id) {
+			IList<string> ids = new List<string>();
+			
+			XPathNodeIterator setIdAttributes = flowEngine.XmlDocumentRules.CreateNavigator().Select("//Set[@id='"
+			                                                                                         + id
+			                                                                                         + "']//Condition/Compare/@leftId | //Set[@id='"
+			                                                                                         + id
+			                                                                                         + "']//Condition/Compare/@rightId");
+			while(setIdAttributes.MoveNext()) {
+				ids.Add(setIdAttributes.Current.Value);
+			}
+			
+			return ids;
+		}
+		
+	}
+}
---
+++ NxBRE3/Test/FlowEngine/TestBackwardChainer.cs
@@ -0,0 +1,55 @@
+﻿namespace NxBRE.Test.FlowEngine {
+	using System;
+	using System.Collections.Generic;
+	
+	using NxBRE.FlowEngine;
+	using NxBRE.FlowEngine.IO;
+	using NxBRE.FlowEngine.Core;
+	
+	using NxBRE.Util;
+
+	using NUnit.Framework;
+	
+	[TestFixture]
+	public class TestBackwardChainer {
+		private IFlowEngine flowEngine;
+		private BackwardChainer backwardChainer;
+		
+		[TestFixtureSetUp]
+		public void InitializeChainer() {
+			string ruleFilesFolder = Parameter.GetString("unittest.ruleml.inputfolder");
+			
+			flowEngine = new BREImpl();
+			flowEngine.Init(new XBusinessRulesFileDriver(ruleFilesFolder + "/car-loan-rules.xbre"));
+			
+			backwardChainer = new BackwardChainer(flowEngine);
+		}
+		
+		[Test]
+		public void SetsForKnownOutcome() {
+			IList<string> sets = backwardChainer.GetSetIdsFromTargetObjectId("IncomeCriteriaOk");
+			Assert.AreEqual(2, sets.Count);
+			Assert.IsTrue(sets.Contains("rule9"));
+			Assert.IsTrue(sets.Contains("rule10"));
+		}
+				
+		[Test]
+		public void SetsForUnknownOutcome() {
+			Assert.AreEqual(0, backwardChainer.GetSetIdsFromTargetObjectId("foo").Count);
+		}
+				
+		[Test]
+		public void SourceObjectsForKnownSet() {
+			IList<string> objects = backwardChainer.GetSourceObjectIdsFromSetId("rule9");
+			Assert.AreEqual(4, objects.Count);
+			Assert.IsTrue(objects.Contains("IsCebEmployee"));
+			Assert.IsTrue(objects.Contains("TotalIncome"));
+		}
+		
+		[Test]
+		public void SourceObjectsForUnknownSet() {
+			Assert.AreEqual(0, backwardChainer.GetSourceObjectIdsFromSetId("foo").Count);
+		}
+		
+	}
+}
