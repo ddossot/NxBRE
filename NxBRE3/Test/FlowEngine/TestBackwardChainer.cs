86f1694a665e96115e5d3a4005f423117a8adbf8
ddossot <ddossot@3573d164-ba16-0410-8823-8bc0bb1327a7>
2008-03-01 19:13:57 +0000

    FR-1776382 Improved BackwardChainer to support RRML.

--- NxBRE3/Test/FlowEngine/TestBackwardChainer.cs
+++ NxBRE3/Test/FlowEngine/TestBackwardChainer.cs
@@ -15,7 +15,7 @@
 		private IFlowEngine flowEngine;
 		private BackwardChainer backwardChainer;
 		
-		[TestFixtureSetUp]
+		[SetUp]
 		public void InitializeChainer() {
 			string ruleFilesFolder = Parameter.GetString("unittest.ruleml.inputfolder");
 			
@@ -26,8 +26,26 @@
 		}
 		
 		[Test]
+		[ExpectedException(typeof(BREException))]
+		public void InvalidRuleBase() {
+			flowEngine = new BREImpl();
+			flowEngine.Init(new XBusinessRulesFileDriver(Parameter.GetString("unittest.inputfile")));
+			backwardChainer = new BackwardChainer(flowEngine);
+		}
+		
+		[Test]
 		public void SetsForKnownOutcome() {
+			Assert.AreEqual(0, backwardChainer.setIdsFromTargetObjectId.Count);
+
 			IList<string> sets = backwardChainer.GetSetIdsFromTargetObjectId("IncomeCriteriaOk");
+			Assert.AreEqual(2, sets.Count);
+			Assert.IsTrue(sets.Contains("rule9"));
+			Assert.IsTrue(sets.Contains("rule10"));
+			
+			Assert.AreEqual(1, backwardChainer.setIdsFromTargetObjectId.Count);
+			
+			// should hit the cache this time
+			sets = backwardChainer.GetSetIdsFromTargetObjectId("IncomeCriteriaOk");
 			Assert.AreEqual(2, sets.Count);
 			Assert.IsTrue(sets.Contains("rule9"));
 			Assert.IsTrue(sets.Contains("rule10"));
@@ -40,7 +58,17 @@
 				
 		[Test]
 		public void SourceObjectsForKnownSet() {
+			Assert.AreEqual(0, backwardChainer.sourceObjectIdsFromSetId.Count);
+			
 			IList<string> objects = backwardChainer.GetSourceObjectIdsFromSetId("rule9");
+			Assert.AreEqual(4, objects.Count);
+			Assert.IsTrue(objects.Contains("IsCebEmployee"));
+			Assert.IsTrue(objects.Contains("TotalIncome"));
+
+			Assert.AreEqual(1, backwardChainer.sourceObjectIdsFromSetId.Count);
+			
+			// should hit the cache this time
+			objects = backwardChainer.GetSourceObjectIdsFromSetId("rule9");
 			Assert.AreEqual(4, objects.Count);
 			Assert.IsTrue(objects.Contains("IsCebEmployee"));
 			Assert.IsTrue(objects.Contains("TotalIncome"));
@@ -49,6 +77,62 @@
 		[Test]
 		public void SourceObjectsForUnknownSet() {
 			Assert.AreEqual(0, backwardChainer.GetSourceObjectIdsFromSetId("foo").Count);
+		}
+		
+		[Test]
+		public void ResolveAlreadyAssertedObject() {
+			flowEngine.RuleContext.SetObject("flowEngine", flowEngine);
+			Assert.AreSame(flowEngine, backwardChainer.Resolve("flowEngine"));
+		}
+		
+		[Test]
+		public void UnresolvableObject() {
+			Assert.IsNull(backwardChainer.Resolve("foo"));
+		}
+		
+		[Test]
+		public void OneLevelProcessingNoOutcome() {
+			Assert.IsNull(backwardChainer.Resolve("IncomeCriteriaOk"), "zero base facts");
+			
+			flowEngine.RuleContext.SetObject("TotalIncome", 16000);
+			Assert.IsNull(backwardChainer.Resolve("IncomeCriteriaOk"), "not enough base facts for rule 10");
+			
+			flowEngine.RuleContext.ResultsMap.Clear();
+			flowEngine.RuleContext.SetObject("IsCebEmployee", true);
+			Assert.IsNull(backwardChainer.Resolve("IncomeCriteriaOk"), "not enough base facts for rule 9");
+		}
+		
+		[Test]
+		public void OneLevelProcessingWithOutcome() {
+			Stack<string> resolutionPath = new Stack<string>();
+			flowEngine.RuleContext.SetObject("IsCebEmployee", true);
+			flowEngine.RuleContext.SetObject("TotalIncome", 12000);
+			Assert.IsTrue((bool)backwardChainer.Resolve("IncomeCriteriaOk", resolutionPath));
+			Assert.AreEqual("({Set:rule9},?IncomeCriteriaOk)", Misc.IListToString<string>(new List<string>(resolutionPath)));
+
+			resolutionPath = new Stack<string>();
+			Assert.IsTrue((bool)backwardChainer.Resolve("IncomeCriteriaOk", resolutionPath));
+			Assert.AreEqual("({RuleContext},?IncomeCriteriaOk)", Misc.IListToString<string>(new List<string>(resolutionPath)));
+
+			resolutionPath = new Stack<string>();
+			flowEngine.RuleContext.ResultsMap.Remove("IncomeCriteriaOk");
+			Assert.IsTrue((bool)backwardChainer.Resolve("IncomeCriteriaOk", resolutionPath));
+			Assert.AreEqual("({Set:rule9},?IncomeCriteriaOk)", Misc.IListToString<string>(new List<string>(resolutionPath)));
+			
+			resolutionPath = new Stack<string>();
+			flowEngine.RuleContext.ResultsMap.Clear();
+			flowEngine.RuleContext.SetObject("IsCebEmployee", false);
+			flowEngine.RuleContext.SetObject("TotalIncome", 16000);
+			Assert.IsTrue((bool)backwardChainer.Resolve("IncomeCriteriaOk", resolutionPath));
+			Assert.AreEqual("({Set:rule10},?IncomeCriteriaOk)", Misc.IListToString<string>(new List<string>(resolutionPath)));
+		}
+		
+		[Test]
+		public void TwoLevelProcessingNoOutcome() {
+			Stack<string> resolutionPath = new Stack<string>();
+			flowEngine.RuleContext.SetObject("TotalIncome", -1);
+			Assert.IsNull(backwardChainer.Resolve("ApproveCarLoan", resolutionPath), "not enough base facts for rule 11");
+			Console.WriteLine(Misc.IListToString<string>(new List<string>(resolutionPath)));
 		}
 		
 	}
