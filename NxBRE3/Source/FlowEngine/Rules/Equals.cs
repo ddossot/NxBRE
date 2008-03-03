4e21ec54275e7bff6b5c504d3a22467305d0800d
ddossot <ddossot@3573d164-ba16-0410-8823-8bc0bb1327a7>
2008-03-03 02:45:25 +0000

    Refactored/sanitized Flow Engine's operators

--- NxBRE3/Source/FlowEngine/Rules/Equals.cs
+++ NxBRE3/Source/FlowEngine/Rules/Equals.cs
@@ -7,14 +7,9 @@ namespace NxBRE.FlowEngine.Rules
 
 	/// <summary> Compares two objects to see if they are equal.
 	/// </summary>
-	/// <author>  Sloan Seaman
-	/// </author>
 	public sealed class Equals : IBREOperator
 	{
-		//TODO refactor to use common comparison abstract class
-		
 		/// <summary>Defines whether an operator is able to deal with empty operands</summary>
-		/// <returns>False for the current operator</returns>
 		public bool AcceptsNulls {
 			get {
 				return false;
@@ -22,31 +17,12 @@ namespace NxBRE.FlowEngine.Rules
 		}
 
 		/// <summary> Checks the two objects to see if they are equal
-		/// *
 		/// </summary>
-		/// <param name="aBRC">The BRERuleContext object containing all the state
-		/// information for use by this method.
-		/// </param>
-		/// <param name="aMap">Not used yet. An IDictionary object containing the parameters from the XML
-		/// </param>
-		/// <param name="aObj">The object (value) to compare against.
-		/// </param>
-		/// <param name="aCompareTo">The object to compare it to.
-		/// </param>
-		/// <returns> True if they meet the condition, False otherwise.
-		/// </returns>
-		public bool ExecuteComparison(IBRERuleContext aBRC, IDictionary aMap, object aObj, object aCompareTo)
-		{
-			if ((aObj is IComparable) && (aCompareTo is IComparable))
-			{
-				if (((IComparable) aObj).CompareTo(aCompareTo) == 0)
-				{
-					return true;
-				}
-				return false;
+		public bool ExecuteComparison(IBRERuleContext aBRC, IDictionary aMap, object aObj, object aCompareTo) {
+			if ((aObj is IComparable) && (aCompareTo is IComparable)) {
+				return ((IComparable) aObj).CompareTo(aCompareTo) == 0;
 			}
-			else
-			{
+			else {
 				return aCompareTo.Equals(aObj);
 			}
 		}
--- NxBRE3/Source/FlowEngine/Rules/GreaterThan.cs
+++ NxBRE3/Source/FlowEngine/Rules/GreaterThan.cs
@@ -1,5 +1,4 @@
-namespace NxBRE.FlowEngine.Rules
-{
+namespace NxBRE.FlowEngine.Rules {
 	using System;
 	using System.Collections;
 
@@ -7,79 +6,9 @@ namespace NxBRE.FlowEngine.Rules
 
 	/// <summary> Compares two objects to see if one is greater than the other
 	/// </summary>
-	/// <author>  Sloan Seaman
-	/// </author>
-	public sealed class GreaterThan : IBREOperator, IInitializable
-	{
-		//TODO refactor & extract comparison abstract class
-		
-		/// <summary>Defines whether an operator is ablt to deal with empty operands</summary>
-		/// <returns>False for the current operator</returns>
-		public bool AcceptsNulls {
-			get {
-				return false;
-			}
-		}
-		
-		/// <summary> Vairable that holds the optimism
-		/// </summary>
-		private bool optimistic = false;
-				
-		/// <summary> Takes a Boolean as its object.  The Boolean represents
-		/// if the object should be pessistic or optimistic when 
-		/// doing a comparison. I.E. if it cannot do a comparison
-		/// does it return true or false.  Default is false..
-		/// *
-		/// </summary>
-		/// <param name="aObj">A Boolean containing the optimism
-		/// 
-		/// </param>
-		public bool Init(object aObj)
-		{
-			if (aObj is System.Boolean)
-			{
-				optimistic = ((System.Boolean) aObj);
-				return true;
-			}
-			return false;
-		}
-		
-		/// <summary> Checks the two objects to see if one is greater than the other
-		/// <P>
-		/// If the objects are not <code>Comparable</code> then we default
-		/// to the optimism that is set.  The default is false.
-		/// *
-		/// </P>
-		/// </summary>
-		/// <param name="aBRC">The BRERuleContext object containing all the state
-		/// information for use by this method.
-		/// </param>
-		/// <param name="aMap">An IDictionary object containing the parameters from the XML
-		/// </param>
-		/// <param name="aObj">The object (value) to compare against.
-		/// </param>
-		/// <param name="aCompareTo">The object to compare it to.
-		/// </param>
-		/// <returns> True if they meet the condition, False otherwise.
-		/// 
-		/// </returns>
-		public bool ExecuteComparison(IBRERuleContext aBRC, IDictionary aMap, object aObj, object aCompareTo)
-		{
-			Init(aMap);
-			
-			if ((aObj is IComparable) && (aCompareTo is IComparable))
-			{
-				if ((! aCompareTo.GetType().IsInstanceOfType(aObj)) 
-				    || (! aCompareTo.GetType().IsSubclassOf(aObj.GetType()))) {
-					aCompareTo = Convert.ChangeType(aCompareTo, aObj.GetType());
-				}
-				
-				return (((IComparable) aObj).CompareTo(aCompareTo) > 0);
-			}
-			else
-			{
-				return optimistic;
-			}
+	public sealed class GreaterThan : AbstractComparisonOperator, IBREOperator {
+		protected override bool AnalyzeCompared(int compare) {
+			return compare > 0;
 		}
 	}
 }
